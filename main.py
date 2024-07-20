4413fdae738e6bb1ad4fccda6c7c59b33c60b25a
Anunaya Pandey <apandey306@gatech.edu>
2024-07-20 16:13:11 +0530

    Adding main api file, functions and data for trendline api

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, Form, Request, status
 from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from typing import List
+from chapter_11_functions import fit_trendline, country_trendline
+from pydantic import BaseModel
 import uvicorn
 
 
@@ -28,6 +31,35 @@ async def hello(request: Request, name: str = Form(...)):
     else:
         print('Request for hello page received with no name or blank name -- redirecting')
         return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
+    
+@app.get("/say_hello/{name}")
+def say_hello(name):
+    return {"Hello": name}
+
+
+class TrendlineInput(BaseModel):
+    timestamps: List[int]
+    data: List[float]
+
+
+# This function will accept data in JSON format as its input. 
+# You can also improve your API documentation very easily by adding 
+# a summary and description to the @app decorator arguments.
+@app.post(
+    "/fit_trendline/",
+    summary="Fit a trendline to any data",
+    description="Provide a list of integer timestamps and a list of floats",
+)
+def calculate_trendline(trendline_input: TrendlineInput):
+    slope, r_squared = fit_trendline(trendline_input.timestamps, trendline_input.data)
+    return {"slope": slope, "r_squared": r_squared}
+
+
+@app.get("/country_trendline/{country}")
+def calculate_country_trendline(country: str):
+    slope, r_squared = country_trendline(country)
+    return {"slope": slope, "r_squared": r_squared}
+
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8000)
